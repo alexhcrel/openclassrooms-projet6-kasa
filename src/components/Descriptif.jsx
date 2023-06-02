3b758b1f803c4dc71ec4c3ef9601b6f8e13d90e3
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-02 16:17:04 +0200

    pas d'avancée

--- src/components/Descriptif.jsx
+++ src/components/Descriptif.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Descriptif = ({props}) => {
-    console.log(props)
+const Descriptif = ({logement}) => {
+    console.log(logement)
    
     return (
         <div className='descriptif'>
