d01d14d61c9b46e2b4bc16083fc42c3e1e9d291d
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-03 13:31:30 +0200

    import des components dans Descriptif

--- src/components/Descriptif.jsx
+++ src/components/Descriptif.jsx
@@ -1,11 +1,69 @@
 import React from "react";
+import Collapse from './Collapse'
 
 const Descriptif = ({ objet }) => {
-  console.log(objet);
+  let tagsArray = [];
+  if (objet && objet.tags) {
+    tagsArray = objet.tags;
+  }
+
+  let equipmentsArray = [];
+  if (objet && objet.equipments) {
+    equipmentsArray = objet.equipments;
+  }
 
   return (
-    <div className="descriptif" key={objet.id.id}>
-      <h2>{objet.title}</h2>
+    <div className="descriptifComplet" key={objet.id.id}>
+      <div className="descriptif_bandeau">
+        <div className="descriptif_bandeau_left">
+          <div>
+            <h1>{objet.title}</h1>
+            <h3>{objet.location}</h3>
+          </div>
+          <div className="tags">
+            {tagsArray.map((tag, index) => (
+              <h4 key={index}>{tag}</h4>
+            ))}
+          </div>
+        </div>
+
+        <div className='descriptif_bandeau_right'>
+            <div className='host'>
+                <h3>{objet.host.name}</h3>
+                <img src={objet.host.picture} alt={objet.title} />
+            </div>
+            <div className = "stars">
+
+            </div>
+        </div>
+
+
+
+
+      </div>
+
+
+      <div className="descriptif_corps">
+      <div>
+        <Collapse title="Description">
+          <p>{objet.description}</p>
+        </Collapse>
+      </div>
+      <div>
+        <Collapse title="Equipements">
+          <p>
+            {equipmentsArray.map((equipment, index) => (
+              <p key={index}>{equipment}</p>
+            ))}
+          </p>
+        </Collapse>
+      </div>
+    </div>
+
+
+
+
+
     </div>
   );
 };
