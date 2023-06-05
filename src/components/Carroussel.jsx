825b57676d75b8b669dec152d086fb321e6c0f14
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-05 17:06:26 +0200

    Flèches carroussel fonctionnelles

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 
 import "../styles/carroussel.css";
+import { useState } from "react";
 
 const Carroussel = ({ objet }) => {
-  const slides = [];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   let picturesArray = [];
   if (objet && objet.pictures) {
     picturesArray = objet.pictures;
   }
 
-  console.log(picturesArray);
+console.log(picturesArray.length)
 
+ const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? picturesArray.length -1 : currentIndex - 1;
+ setCurrentIndex(newIndex)
+ }
+
+const goToNext = () => {
+    const isLastSlide = currentIndex === picturesArray.length -1
+    const newIndex = isLastSlide ? 0 : currentIndex +1;
+setCurrentIndex(newIndex)
+
+
+}
   return (
     <div className="sliderStyles">
       <div
         className="slideStyles"
-        style={{ backgroundImage: `url(${picturesArray[0]})`}}
+        style={{ backgroundImage: `url(${picturesArray[currentIndex]})`}}
       >
-        <div className="arrow leftArrow">‹</div>
+        <div className="arrow leftArrow" onClick={goToPrevious}>‹</div>
 
-        <div className="arrow rightArrow">›</div>
+        <div className="arrow rightArrow"onClick={goToNext}>›</div>
       </div>
     </div>
 
