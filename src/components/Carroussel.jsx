0f07aafba9ada389ff71155d8b5c4d68dc6fb390
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-06 09:02:54 +0200

    carroussel fonctionnel et mis en page

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -4,17 +4,10 @@ import "../styles/carroussel.css";
 import { useState } from "react";
 
 const Carroussel = ({ objet }) => {
+  // const isUnique = () =>{
 
- 
-        const isUnique = () =>{
-            if (picturesArray.length == 1) {
-                console.log("unique")
-            } else {
-                console.log("plusiuers")
-            }
-        }  
-  
-    
+  // }
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   let picturesArray = [];
@@ -22,36 +15,40 @@ const Carroussel = ({ objet }) => {
     picturesArray = objet.pictures;
   }
 
- const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? picturesArray.length -1 : currentIndex - 1;
- setCurrentIndex(newIndex)
- }
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? picturesArray.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
-const goToNext = () => {
-    const isLastSlide = currentIndex === picturesArray.length -1
-    const newIndex = isLastSlide ? 0 : currentIndex +1;
-setCurrentIndex(newIndex)
-
-}
-
-
-
+  const goToNext = () => {
+    const isLastSlide = currentIndex === picturesArray.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   return (
-    <div className="sliderStyles" >
+    <div className="sliderStyles">
       <div
         className="slideStyles"
-        style={{ backgroundImage: `url(${picturesArray[currentIndex]})`}}
-        >
-
-        <div className="arrow leftArrow" onClick={goToPrevious}>‹</div>
-
-        <div className="arrow rightArrow" onClick={goToNext}>›</div>
+        style={{ backgroundImage: `url(${picturesArray[currentIndex]})` }}
+      >
+        {picturesArray.length !== 1 && (
+          <React.Fragment>
+            
+          
+              <div className="arrow leftArrow" onClick={goToPrevious}>
+                ‹
+              </div>
+              <div className="arrow rightArrow" onClick={goToNext}>
+                ›
+              </div>
+              
+           
+          </React.Fragment>
+        )}
       </div>
     </div>
-   
- 
   );
 };
 
