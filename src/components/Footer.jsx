67aedbf1e6268ff90a75937fe3293838ab502838
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-08 09:27:44 +0200

    logo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
-            <div className="logo_white">
-            <img src ="../logo_kasa_white/vector_k_big.png" />
-            <img src ="../logo_kasa_white/vector_maison.png" />
-            <img src ="../logo_kasa_white/vector_s.png" />
-            <img src ="../logo_kasa_white/vector_a.png" />
-            </div>
+     
+            <img src ="../logo/kasa_white.svg" />
+
+            
+         
             <div>
             © 2020 Kasa. All rights reserved
             </div>
