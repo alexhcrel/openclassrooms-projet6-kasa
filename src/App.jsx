abb2e24ec3925f702d82183c3a3f0d73f4148888
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-09 16:18:14 +0200

    mise-en page App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './styles/index.css';
+import "./styles/index.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Fichelogement from "./pages/FicheLogement";
 
 const App = () => {
-
   return (
     <div>
-
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path={`/fichelogement/:logementId`} element={<Fichelogement />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Error />} />      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path={`/fichelogement/:logementId`}
+            element={<Fichelogement />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />{" "}
+        </Routes>
+      </BrowserRouter>
     </div>
-  
   );
 };
-
 
 export default App;
