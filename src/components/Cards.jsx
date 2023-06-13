36f18dd204ff74a90bd1cf2e39efb7725fb8f662
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-13 10:41:01 +0200

    suppression d'un commentaire

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,7 +13,7 @@ const Cards = ({ logement }) => {
       state={{ logement }}
     >
       <div
-        className="card" /*onClick={() => handleClick("http://localhost:3000/fichelogement")}*/
+        className="card"
       >
         <div className="cardlogement" style={{
         backgroundImage:`linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url(${logement.cover})`,
