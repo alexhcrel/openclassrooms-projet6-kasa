820fd3738ff83bd884a96e7e576339f0d258f722
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-06-15 11:42:18 +0200

    Composant_banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/banner.css"
 
-const Banner_accueil = () => {
+const Banner = () => {
     return (
         <div className="Banner Banner_accueil">
             <h1>Chez vous, partout et ailleurs</h1>
@@ -9,4 +9,13 @@ const Banner_accueil = () => {
     );
 };
 
-export default Banner_accueil;
+export default Banner;
+
+
+
+
+
+
+
+
+
